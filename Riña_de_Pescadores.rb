de03da5cfe6a76c8a20a264c573b4390d7eb7b8c
refo44 <refo44@gmail.com>
2016-08-18 05:54:19 -0400

    modificado:    "Ri\303\261a_de_Pescadores.rb":
      agregada primera version del tablero inicial
    
    nuevo archivo: utf8_symbols.markdown

--- Riña_de_Pescadores.rb
+++ Riña_de_Pescadores.rb
@@ -39,6 +39,7 @@ end
  def login_juego
  	# Este procedimiento
  	# solicita el nombre del jugador
+ 	
  	puts "RIÑA DE PESCADORES"
  	puts
  	print "Introduzca su nombre : "
@@ -48,35 +49,100 @@ end
 
 def seleccion_num_barcos
 
-num_barcos = 10 # inicializado por defecto
+  num_barcos = 10 # inicializado por defecto
 
-loop do
-	
- system('clear')
- puts
-  if num_barcos < 4 || num_barcos > 11
-   puts "Debe escoger un numero entre 4 y 11"
-   puts
+  loop do
+  
+    system('clear')
+    puts
+     if num_barcos < 4 || num_barcos > 11
+      puts "Debe escoger un numero entre 4 y 11"
+      puts
+     end
+    
+     print "  ¿Con cuantos barcos desea jugar?: "
+     num_barcos = gets.chomp.to_i 
+     puts
+    
+     break if num_barcos > 3 && num_barcos < 11
   end
 
-  print "  ¿Con cuantos barcos desea jugar?: "
-  num_barcos = gets.chomp.to_i 
-  puts
+end
 
-  break if num_barcos>3 && num_barcos <11
+def fill_tablero(s,n_length)
+
+# Esta funcion retorna una matriz  NxN
+# con el elemento s en todas sus entradas
+
+  tablero = []
+
+  for i in 0..n_length-1 do
+
+    tablero[i] = []
+
+    for j in 0..n_length-1 do
+
+    tablero[i][j] = s 
+
+    end
+
+  end
+
+  return tablero
+
+end
+
+def mostrar_tablero(m)
+
+	print "  "
+
+    for k in 0..m.length-1 do
+      print "%2d" % k 
+    end
+
+    puts
+    puts
+   
+    row_tags = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+  
+  for i in 0..m.length-1 do
+  
+  	print "#{row_tags[i]} " 
+  
+    for j in 0..m.length-1 do
+  
+     print "%2s" % m[i][j]
+  
+    end
+    print " "
+    puts
+  
+  end
+
+puts
+
 end
 
 
-end
-
-
-def put_barcos
+def set_barcos
 # Pregunta al jugador el numero barcos 
 # que se utilizaran en la partida
 
-  seleccion_num_barcos
+  #seleccion_num_barcos
+ 
 
-end  
+# incializamos el tablero de barcos
+# 
+ num_barcos = 10
+
+barcos_jugador = fill_tablero("\u25A7",10)
+
+mostrar_tablero(barcos_jugador)
+
+
+
+end 
+
 
 ###############  main ###########################################
 
@@ -84,7 +150,7 @@ def main
 
   #bienvenida_process 
   #login_process
-  put_barcos_process
+  set_barcos
 
 
 end
