0d0278d96f674f9665d15751f3851282bf1f41b7
refo44 <refo44@gmail.com>
2016-08-18 16:34:57 -0400

    modificado:    "Ri\303\261a_de_Pescadores.rb"
    eliminadao: funcion bienvenida_msg
    agregado: anykey, titulo

--- Riña_de_Pescadores.rb
+++ Riña_de_Pescadores.rb
@@ -15,17 +15,9 @@
 	
   return t
 end
- 
-def bienvenida_msj
-# Este procedimiento
-# Muestra la pantalla de bienvenida del juego
- 	
-  system('clear') 
 
-  puts
-  puts "RIÑA DE PESCADORES"
-  puts
-  puts "Presione cualquier tecla para iniciar (s para salir): "
+def anytecla
+   puts "Presione cualquier tecla para continuar (s para salir del juego): "
   tecla = get_keypressed  
 
   system('clear') 
@@ -33,6 +25,45 @@ def bienvenida_msj
   if tecla.downcase == "s"  
     exit
   end
+end
+
+def titulo
+ system('clear') 
+
+  puts
+  puts "RIÑA DE PESCADORES"
+  put
+
+end
+ 
+def bienvenida_msj
+# Este procedimiento
+# Muestra la pantalla de bienvenida del juego
+ 	
+ 
+
+end
+
+def loading_partida
+
+  system('clear')
+  puts "RIÑA DE PESCADORES"
+  puts "comenzando partida."
+  sleep 0.4
+  system('clear')
+  puts "RIÑA DE PESCADORES"
+  puts "comenzando partida.."
+  sleep 0.4
+  system('clear')
+  puts "RIÑA DE PESCADORES"
+  puts "comenzando partida..."
+  puts
+  sleep 0.4
+  system('clear')
+  puts "RIÑA DE PESCADORES"
+  puts "comenzando partida...."
+  sleep 0.4
+  system('clear')
 
 end
 
@@ -55,8 +86,8 @@ def select_num_barcos
   
     system('clear')
     puts
-     if num_barcos < 4 || num_barcos > 11
-      puts "Debe escoger un numero entre 4 y 11"
+     if num_barcos < 4 || num_barcos > 10
+      puts "Debe escoger un numero entre 4 y 10"
       puts
      end
     
@@ -132,28 +163,64 @@ def colocar_en_tablero(tablero,s)
 # coloca un elemento s en el tablero m
 #en la posicion (a,b)
 
-print "Introduzca posicion: "
-a = get_keypressed.to_i 
-print a 
-b = get_keypressed.to_i
-print b
-sleep 0.4
+  ocupado = false
 
-tablero[a][b] = s
+  loop do
 
-system ('clear')
+  	if ocupado == true then
+
+     puts
+     puts "La posicion ya esta ocupada"
+     puts 
+
+  	end
+  
+    print "Introduzca posicion: "
+    a = get_keypressed.to_i 
+    print a 
+    b = get_keypressed.to_i
+    print b
+    sleep 0.4
+    system('clear')
+    
+    if tablero[a][b] != s
+
+      tablero[a][b]= s
+
+      ocupado = false
+
+    else
+
+      ocupado = true 
+
+      mostrar_tablero(barcos_jugador)     
+
+    end
+  
+    break if ocupado == false 
+  end
+  
+  system('clear')
 
 end
 
+
 def colocar_en_tablero_aleatorio(tablero,s)
 
-a = rand(tablero.length) + 1
-b = rand(tablero.length) + 1 
+ocupado = true
 
-puts "a: #{a}  b: #{b}"
-puts
+loop do 
+a = rand(tablero.length) 
+b = rand(tablero.length) 
 
-tablero[a][b] = s
+if tablero[a][b] != s
+
+  tablero[a][b] = s
+  ocupado = false
+end
+
+break if ocupado == false
+end
 
 system ('clear')
 
@@ -163,7 +230,7 @@ def set_barcos_aleatorio(tablero, n)
  #configuracion inicial aleatoria
  # de n barcos en el tablero
  
- for k in 0..n do 
+ for k in 1..n do 
 colocar_en_tablero_aleatorio(tablero, "\u2693")
 end
 
@@ -174,7 +241,7 @@ def set_barcos(num_barcos)
 # Pregunta al jugador el numero barcos 
 # que se utilizaran en la partida
 
-  #seleccion_num_barcos
+
  
 
 # incializamos el tablero de barcos 
@@ -198,6 +265,8 @@ puts
 puts"Barcos compu"
 mostrar_tablero(barcos_compu)
 
+gets
+system('clear')
 end 
 
 
@@ -207,13 +276,22 @@ def main
 
 
 
+
   num_symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   letras_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+  simbolo = [ "\u25a7", "\u2635","\u2693"  ]
 
-  bienvenida_msj 
+  titulo
+  anytecla
+  
   login_menu
   num_barcos = select_num_barcos
+  #num_barcos = 4 #modo debug
   set_barcos(num_barcos)
+  
+  loading_partida
+
+
 end
 
 main
